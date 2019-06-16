4db4a7a5c0faadef18baaaa576d5e2a89e44b4c9
MasterMoritz <MasterMoritz@users.noreply.github.com>
2019-06-16 19:57:22 +0200

    add unit tests for auth/login guard

--- src/app/authentication/guards/login.guard.spec.ts
+++ src/app/authentication/guards/login.guard.spec.ts
@@ -1,15 +1,49 @@
-import { TestBed, async, inject } from '@angular/core/testing';
-
 import { LoginGuard } from './login.guard';
 
-describe('LoginGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [LoginGuard]
+class MockRouter {
+  parseUrl(path: string) { }
+}
+class MockNext {
+
+}
+class MockRouteState {
+
+}
+
+fdescribe('LoginGuard', () => {
+  describe('canActivate', () => {
+    let loginGuard: LoginGuard;
+    let router;
+    let next;
+    let state;
+
+    it('return true for a logged out user', () => {
+      const store = jasmine.createSpyObj('store', ['selectSnapshot']);
+      store.selectSnapshot.and.callFake((param) => false);
+
+      //these are not needed yet
+      next = new MockNext();
+      state = new MockRouteState();
+
+      router = new MockRouter();
+      loginGuard = new LoginGuard(store, router);
+
+      expect(loginGuard.canActivate(next, state)).toEqual(true);
+    });
+
+    it('should navigate to homepage for a logged in user', () => {
+      const store = jasmine.createSpyObj('store', ['selectSnapshot']);
+      store.selectSnapshot.and.callFake((param) => true);
+
+      next = new MockNext();
+      state = new MockRouteState();
+
+      router = new MockRouter();
+      loginGuard = new LoginGuard(store, router);
+
+      spyOn(router, 'parseUrl');
+      expect(loginGuard.canActivate(next, state)).not.toEqual(true);
+      expect(router.parseUrl).toHaveBeenCalledWith('');
     });
   });
-
-  it('should ...', inject([LoginGuard], (guard: LoginGuard) => {
-    expect(guard).toBeTruthy();
-  }));
 });
