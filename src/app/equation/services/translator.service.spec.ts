881618c055741db5829ff7f20a4e3479b0a8179d
MasterMoritz <MasterMoritz@users.noreply.github.com>
2019-06-16 22:22:04 +0200

    add unit test for translator service

--- src/app/equation/services/translator.service.spec.ts
+++ src/app/equation/services/translator.service.spec.ts
@@ -9,4 +9,18 @@ describe('TranslatorService', () => {
     const service: TranslatorService = TestBed.get(TranslatorService);
     expect(service).toBeTruthy();
   });
+
+  describe('asciiMath to TeX translation', () => {
+
+    it('sum_(i=1)^n i^3=((n(n+1))/2)^2', () => {
+      const service: TranslatorService = TestBed.get(TranslatorService);
+      expect(service.parse('sum_(i=1)^n i^3=((n(n+1))/2)^2')).toEqual('\\sum_{i = 1}^{n} i^{3} = \\left( \\frac{n \\left( n + 1 \\right )}{2} \\right )^{2}');
+    });
+
+    it('sqrt((a+b) / c)', () => {
+      const service: TranslatorService = TestBed.get(TranslatorService);
+      expect(service.parse('sqrt((a+b) / c)')).toEqual('\\sqrt{\\frac{a + b}{c}}');
+    });
+  })
+  
 });
