571cbbb3336dc3ae82c3f7a6bcb282967a654b2d
MasterMoritz <MasterMoritz@users.noreply.github.com>
2019-06-16 21:55:33 +0200

    unit test auth service

--- src/app/authentication/services/auth.service.spec.ts
+++ src/app/authentication/services/auth.service.spec.ts
@@ -1,33 +1,116 @@
+import * as firebase from 'firebase';
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
-import { LoginComponent } from '../components/login/login.component';
-import { auth } from 'firebase';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
-import { Store, Actions } from '@ngxs/store';
+import { Actions } from '@ngxs/store';
 import { ToasterService } from 'angular2-toaster';
+import { Observable, BehaviorSubject } from 'rxjs';
 
-describe('Auth Service and Login', () => {
+
+const credentialsMock = {
+  email: 'abc@123.com',
+  password: 'password'
+};
+
+const userMock = {
+  uid: 'ABC123',
+  email: credentialsMock.email,
+};
+
+const fakeAuthState = new BehaviorSubject(null); // <= Pay attention to this guy
+
+const fakeSignInHandler = (email, password): Promise<any> => {
+  fakeAuthState.next(userMock);
+  return Promise.resolve(userMock);
+};
+
+const fakeSignOutHandler = (): Promise<any> => {
+  fakeAuthState.next(null);
+  return Promise.resolve();
+};
+
+const routerStub = {
+  parseUrl: (path: string) => { },
+  navigate: (path: string) => { },
+};
+
+const toasterStub = {
+  pop: (type: string, title: string, body: string) => { }
+};
+
+const actionsTest = new Observable<any>();
+
+const angularFireAuthStub = {
+  authState: fakeAuthState,
+  auth: {
+    createUserWithEmailAndPassword: jasmine
+      .createSpy('createUserWithEmailAndPassword')
+      .and
+      .callFake(fakeSignInHandler),
+    signInWithEmailAndPassword: jasmine
+      .createSpy('signInWithEmailAndPassword')
+      .and
+      .callFake(fakeSignInHandler),
+    signOut: jasmine
+      .createSpy('signOut')
+      .and
+      .callFake(fakeSignOutHandler),
+  },
+};
+
+describe('Auth Service', () => {
   let service: AuthService;
-  let spy: any;
+  let afAuth: AngularFireAuth;
 
   beforeEach(() => {
-
-    service = jasmine.createSpyObj('authService', [auth]);
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        { provide: AngularFireAuth, useValue: angularFireAuthStub },
+        { provide: Router, useValue: routerStub },
+        { provide: ToasterService, useValue: toasterStub },
+        { provide: Actions, useValue: actionsTest },
+      ],
     });
 
-
-
+    service = TestBed.get(AuthService);
+    afAuth = TestBed.get(AngularFireAuth);
+    spyOn(firebase, "auth").and.returnValue(angularFireAuthStub.auth);
+  });
 
   afterEach(() => {
-                    service = null;
+    fakeAuthState.next(null);
+  });
+
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Login', () => {
+    it('should call the signInWithEmailAndPassword auth fire method with provided credentials when logging in with email and password', () => {
+      service.loginWithEmail(credentialsMock.email, credentialsMock.password);
+
+      expect(afAuth.auth.signInWithEmailAndPassword)
+        .toHaveBeenCalledWith(credentialsMock.email, credentialsMock.password);
     });
+  });
+  describe('Register', () => {
+    it('should call the createUserWithEmailAndPassword auth fire method with provided credentials when registering new user with email and password', () => {
+      service.registerAndLogin(credentialsMock.email, credentialsMock.password);
 
+      expect(afAuth.auth.createUserWithEmailAndPassword)
+        .toHaveBeenCalledWith(credentialsMock.email, credentialsMock.password);
+    });
+  });
+  describe('Logout', () => {
+    it('should call the signOut auth fire method once when logging out ', () => {
+      service.logout();
 
-  it('loginWithEmail returns false when the user is not authenticated', () => {
-    spy = spyOn(service, 'loginWithEmail').and.returnValues('test@test.de', '1233456');
-    expect(service.loginWithEmail).toHaveBeenCalled();
-
+      expect(afAuth.auth.signOut)
+        .toHaveBeenCalledTimes(1);
+    });
   });
 });
