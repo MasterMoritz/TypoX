ef47e3e6bf26061607861c186dca8eaadafc371a
Carina <coerryro@gmail.com>
2019-06-16 03:18:43 +0200

    made deployment ready

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/authentication/services/auth.service';
+import { Select, Store } from '@ngxs/store';
+import { AuthState } from 'src/app/authentication/store/auth.state';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -7,10 +11,13 @@ import { AuthService } from 'src/app/authentication/services/auth.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Select(AuthState.loggedIn) loggedIn$ : Observable<boolean>;
 
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
+
+
 
 }
